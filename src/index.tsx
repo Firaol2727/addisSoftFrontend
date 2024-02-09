bd86178a1299594a24af78c1121e45509f82890b
unknown <fraol.getachew@lersha.com>
2024-02-09 15:34:34 +0300

    minor changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import * as ReactDOM from 'react-dom';
 // Pages 
 
 import Home from './pages/home/Home.tsx';
@@ -10,24 +9,27 @@ import EditSong from './pages/edit/Edit.tsx';
 import { BrowserRouter as Router, Route,  Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store.ts';
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route  path="/" element={<Home/>} />
-          <Route path="/editSong/:id" element={<EditSong/>} />
-          <Route path="/statics" element={<Statics/>} />
-        </Routes>
-      </Router>
-    </Provider>
-  </React.StrictMode>
+      <Provider store={store}>
+        <Router>
+          <Routes>
+            <Route  path="/" element={<Home/>} />
+            <Route path="/editSong/:id" element={<EditSong/>} />
+            <Route path="/statics" element={<Statics/>} />
+          </Routes>
+        </Router>
+      </Provider>
+    </React.StrictMode>,document.getElementById('root') as HTMLElement
 
-);
+)
+// const root = ReactDOM.c(
+//   document.getElementById('root') as HTMLElement
+// );
+
+// root.render(
+  
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
